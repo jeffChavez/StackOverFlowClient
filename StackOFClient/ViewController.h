d30e5b7d64556a30f93605734b2177497d51a60c
Jeff Chavez <jeffchavez8588@gmail.com>
2014-11-10 15:54:57 -0800

    Initial Commit

---
+++ StackOFClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOFClient
+//
+//  Created by Jeff Chavez on 11/10/14.
+//  Copyright (c) 2014 Jeff Chavez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOFClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StackOFClient
+//
+//  Created by Jeff Chavez on 11/10/14.
+//  Copyright (c) 2014 Jeff Chavez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
